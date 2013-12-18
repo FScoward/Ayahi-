be7a206b33dd8252633394c6f1fc03fed7a10081
FScoward <endlick1989@yahoo.co.jp>
2013-12-19 00:46:49 +0900

    cssの追加 不要コメントの削除 ソースのインデントの修正

--- js/popup.js
+++ js/popup.js
@@ -1,24 +1,6 @@
 // chrome-extension://pdekfdkgogcakpbmffdjmlaedhblbjbc/html/popup.html
 $(function(){
-	/*
-	$.ajax({
-		url: 'http://animage.tumblr.com/tagged/',
-		type: 'GET',
-		cache: 'false',
-
-		// データのロード完了時の処理
-        success: function(res) {
-        	alert("success");
-
-        	content = $(res.responseText).text();
-        	$("div").append(content);
-        },
-        fail: function() {
-        	alert("alert");
-        }
-	})
-*/
-var xhr = new XMLHttpRequest;
+	var xhr = new XMLHttpRequest;
 	xhr.onload = console.log.bind(console);
 	xhr.open('GET', 'http://animage.tumblr.com/tagged/%E9%AB%98%E5%9E%A3%E5%BD%A9%E9%99%BD');
 	xhr.send();
